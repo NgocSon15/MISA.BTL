be8541882309e77ed6b216155efe948586ef160a
NgocSon15 <ngocson1562k@gmail.com>
2020-12-29 23:04:08 +0700

    Add project files.

---
+++ MISA.BTL.Web/js/common/common.js
@@ -0,0 +1,33 @@
+﻿/**
+ * Format dữ liệu ngày tháng sang ngày/tháng/năm
+ * @param {any} date tham số có kiểu dữ liệu bất kỳ
+ * CreatedBy: NNSON (29/12/2020)
+ */
+function formatDate(date) {
+    var date = new Date(date);
+    if (Number.isNaN(date.getTime())) {
+        return "";
+    }
+    else {
+        var day = date.getDate();
+        if (day < 10) day = '0' + day;
+        var month = date.getMonth() + 1;
+        if (month < 10) month = '0' + month;
+        var year = date.getFullYear();
+        return day + '/' + month + '/' + year;
+    }
+}
+
+/**
+ * Hàm định dạng hiển thị tiền tệ
+ * @param {Number} money Số tiền
+ * CreatedBy: NNSON (29/12/2020)
+ */
+function formatMoney(money) {
+    if (money == null) {
+        return "";
+    } else {
+        var num = money.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        return num;
+    }
+}
---
+++ MISA.BTL.Web/js/page/employee.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+/**
+ * Load dữ liệu
+ * CreatedBy: NNSON (29/12/2020)
+ */
+function loadData() {
+    // Lấy dữ liệu về:
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET",
+    }).done(function (res) {
+        var data = res;
+        $.each(data, function (index, item) {
+            var dateOfBirth = item['DateOfBirth'];
+            dateOfBirth = formatDate(dateOfBirth);
+            var salary = item['Salary'];
+            salary = formatMoney(salary);
+            var tr = $(` <tr>
+                        <td style="max-width: 90px;" title="${item['EmployeeCode']}">${item['EmployeeCode']}</td>
+                        <td style="max-width: 120px;" title="${item['FullName']}">${item['FullName']}</td>
+                        <td style="max-width: 70px;" title="${item['GenderName']}">${item['GenderName']}</td>
+                        <td style="max-width: 100px; text-align: center;" title="${dateOfBirth}">${dateOfBirth}</td>
+                        <td style="max-width: 120px;" title="${item['PhoneNumber']}">${item['PhoneNumber']}</td>
+                        <td style="max-width: 200px;" title="${item['Email']}">${item['Email'] || ""}</td>
+                        <td style="max-width: 80px;" title="${item['PositionName']}">${item['PositionName'] || ""}</td>
+                        <td style="max-width: 100px;" title="${item['DepartmentName']}">${item['DepartmentName'] || ""}</td>
+                        <td style="max-width: 100px; text-align: right;" title="${salary}">${salary}</td>
+                        <td style="max-width: 100px;" title="${item['WorkStatusName']}">${item['WorkStatusName'] }</td>
+                    </tr>`);
+            $('table tbody').append(tr);
+        })
+        debugger;
+    }).fail(function (res) {
+
+    })
+}
